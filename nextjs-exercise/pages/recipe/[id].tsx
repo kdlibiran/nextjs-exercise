04b3e1de573eb5b8c867bcb0b09f0a9efc2634d7
Kyle <70441111+kdlibiran@users.noreply.github.com>
2024-09-09 21:45:36 +0800

    Revert "Chore: Add Style"

--- nextjs-exercise/pages/recipe/[id].tsx
+++ nextjs-exercise/pages/recipe/[id].tsx
@@ -2,23 +2,17 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { iRecipe } from "@/types/recipe";
 import RecipeCard from "@/components/recipeCard";
-import Modal from "@/components/modal";
 
 export default function RecipePage() {
   const router = useRouter();
   const { id } = router.query;
   const [recipe, setRecipe] = useState<iRecipe | null>(null);
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   const getRecipe = async (id: string) => {
     const res = await fetch(`/api/recipe/${id}`);
     const data = await res.json();
     return data;
   };  
-
-  const handleImageClick = (image: string) => {
-    setSelectedImage(image);
-  };
 
   useEffect(() => {
     if (id) {
@@ -34,20 +28,15 @@ export default function RecipePage() {
   }
 
   return (
-    <>
-      {selectedImage && (
-        <Modal image={selectedImage} onClose={() => setSelectedImage(null)} />
-      )}
-      <div className="min-h-screen flex flex-col justify-center items-center relative">
-        <div className="absolute top-4 left-4">
-            <button className="text-slate-500 hover:text-blue-700 transition-colors duration-300" onClick={() => router.back()}>
-              ← Back to Recipes
-            </button>
-        </div>
-        <div className="max-w-2xl w-full mx-auto p-4">
-          <RecipeCard recipe={recipe} detailed onImageClick={handleImageClick} />
-        </div>
+    <div className="min-h-screen flex flex-col justify-center items-center relative">
+      <div className="absolute top-4 left-4">
+          <button className="text-blue-500 hover:text-blue-700 transition-colors duration-300" onClick={() => router.back()}>
+            ← Back to Recipes
+          </button>
       </div>
-    </>
+      <div className="max-w-2xl w-full mx-auto p-4">
+        <RecipeCard recipe={recipe} detailed />
+      </div>
+    </div>
   );
 }
